0b45b854bdad25420228945c710a9818122645f0
Devin Pastoor <devin.pastoor@gmail.com>
2018-09-29 09:21:23 -0400

    parse GH packages too

--- packrat/parse_lockfile.go
+++ packrat/parse_lockfile.go
@@ -36,7 +36,37 @@ func ParsePackageReqs(b []byte) PackageReqs {
 		case bytes.Equal(fe[0], []byte("Requires")):
 			pr.Requires = strings.Fields(string(bytes.Replace(fe[1], []byte(","), []byte(" "), -1)))
 		default:
-			fmt.Printf("unrecognized fields %s-%s\n", fe[0], fe[1])
+			continue
+		}
+	}
+	return pr
+}
+
+func ParsePackageReqsGH(b []byte) GithubPackageReqs {
+	pr := GithubPackageReqs{}
+	pr.Reqs = ParsePackageReqs(b)
+	lines := bytes.Split(b, []byte("\n"))
+	for i, f := range lines {
+		if len(f) == 0 {
+			// empty line
+			continue
+		}
+		fe := bytes.SplitN(f, []byte(":"), 2)
+		if len(fe) != 2 {
+			fmt.Printf("bad line %d nelements %d : %s", i, len(fe), f)
+			continue
+		}
+		switch {
+		case bytes.Equal(fe[0], []byte("GithubRef")):
+			pr.GithubRef = trimmedString(fe[1])
+		case bytes.Equal(fe[0], []byte("GithubRepo")):
+			pr.GithubRepo = trimmedString(fe[1])
+		case bytes.Equal(fe[0], []byte("GithubSha1")):
+			pr.GithubSha1 = trimmedString(fe[1])
+		case bytes.Equal(fe[0], []byte("GithubUsername")):
+			pr.GithubUsername = trimmedString(fe[1])
+		default:
+			continue
 		}
 	}
 	return pr
