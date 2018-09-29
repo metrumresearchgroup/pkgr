a98d1bb8189341d0aecfeb5530b2db5dac129c29
Devin Pastoor <devin.pastoor@gmail.com>
2018-09-29 09:09:39 -0400

    additional lockfile structs for github and lockfile itself

--- packrat/structs.go
+++ packrat/structs.go
@@ -8,3 +8,25 @@ type PackageReqs struct {
 	Hash     string
 	Requires []string
 }
+
+// GithubPackageReqs also identifies the
+type GithubPackageReqs struct {
+	Reqs           PackageReqs
+	GithubRepo     string
+	GithubUsername string
+	GithubRef      string
+	GithubSha1     string
+}
+
+// PackratMetadata contains the metadata about the packrat lock file
+type PackratMetadata struct {
+	Format   float32
+	Version  string
+	RVersion string
+	Repos    []string
+}
+type LockFile struct {
+	Metadata PackratMetadata
+	CRANlike []PackageReqs
+	Github   []GithubPackageReqs
+}
