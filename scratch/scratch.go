e3d5a749fe5a6d0bdae237eedeaf663497b552e0
Devin Pastoor <devin.pastoor@gmail.com>
2018-10-04 11:02:36 -0400

    testing new cli args setup

--- scratch/scratch.go
+++ scratch/scratch.go
@@ -2,29 +2,17 @@ package main
 
 import (
 	"fmt"
-	"reflect"
 
 	"github.com/dpastoor/rpackagemanager/rcmd"
 )
 
 func main() {
-	ia := rcmd.InstallArgs{Library: "some/path"}
-	t := reflect.TypeOf(ia)
+	// ia := rcmd.InstallArgs{Library: "some/path"}
+	ia := rcmd.NewDefaultInstallArgs()
 
-	// Get the type and kind of our user variable
-	fmt.Println("Type:", t.Name())
-	fmt.Println("Kind:", t.Kind())
-
-	// Iterate over all available fields and read the tag value
-	for i := 0; i < t.NumField(); i++ {
-		// Get the field, returns https://golang.org/pkg/reflect/#StructField
-		field := t.Field(i)
-		tag := field.Tag.Get("rcmd")
-		if field.Name == "Library" {
-			tag = fmt.Sprintf(tag, ia.Library)
-		}
-		fmt.Printf("%d. %v (%v), tag: '--%v'\n", i+1, field.Name, field.Type.Name(), tag)
-	}
-	args := ia.CliArgs()
-	fmt.Println(args)
+	fmt.Println(ia.CliArgs())
+	ia.Clean = true
+	fmt.Println(ia.CliArgs())
+	ia.Library = "path/to/lib"
+	fmt.Println(ia.CliArgs())
 }
