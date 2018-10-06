08da7e7890d1bdce10dd21bd7bf20d06eb5aeb50
Devin Pastoor <devin.pastoor@gmail.com>
2018-10-06 15:38:02 -0400

    fix bug where no specified R path causes panic
    if RSetting{} then the R() path returned would be "", hence cmd would
    have empty string as the cmd and panic.
    
    Now default to "R" on the system if not specified

--- rcmd/Rsettings.go
+++ rcmd/Rsettings.go
@@ -7,12 +7,16 @@ import (
 
 // R provides a cleaned path to the R executable
 func (rs RSettings) R() string {
+	rpath := rs.Rpath
+	if rpath == "" {
+		return ("R")
+	}
 	// TODO: check if this could have problems with trailing slash on windows
 	// TODO: better to use something like filepath.clean? would that sanitize better?
 
 	// Need to trim trailing slash as will form the R CMD syntax
 	// eg /path/to/R CMD, so can't have /path/to/R/ CMD
-	return strings.TrimSuffix(rs.Rpath, "/")
+	return strings.TrimSuffix(rpath, "/")
 }
 
 // LibPathsEnv returns the libpaths formatted in the style to be set as an environment variable
