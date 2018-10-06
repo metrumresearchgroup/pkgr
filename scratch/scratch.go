87d40a3c00d3155b579478bd630cdd245d6e987b
Devin Pastoor <devin.pastoor@gmail.com>
2018-10-06 15:46:03 -0400

    trying install

--- scratch/scratch.go
+++ scratch/scratch.go
@@ -4,15 +4,25 @@ import (
 	"fmt"
 
 	"github.com/dpastoor/rpackagemanager/rcmd"
+	"github.com/sirupsen/logrus"
+	"github.com/spf13/afero"
 )
 
 func main() {
 	// ia := rcmd.InstallArgs{Library: "some/path"}
 	ia := rcmd.NewDefaultInstallArgs()
+	ia.Library = "../integration_tests/lib"
+	fmt.Println(ia.CliArgs())
+	appFS := afero.NewOsFs()
+	lg := logrus.New()
 
-	fmt.Println(ia.CliArgs())
-	ia.Clean = true
-	fmt.Println(ia.CliArgs())
-	ia.Library = "path/to/lib"
-	fmt.Println(ia.CliArgs())
+	lg.Level = logrus.DebugLevel
+	res, err := rcmd.Install(appFS,
+		"../integration_tests/src/test1_0.0.1.tar.gz",
+		ia,
+		rcmd.RSettings{},
+		rcmd.ExecSettings{},
+		lg)
+	fmt.Println("res: ", res)
+	fmt.Println("err: ", err)
 }
