7e9b29ef99b834fe71153aa79b38954d48040407
Devin Pastoor <devin.pastoor@gmail.com>
2018-10-07 13:01:35 -0400

    transition to installbinary to get same behavior

--- scratch/scratch.go
+++ scratch/scratch.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"os"
 
 	"github.com/dpastoor/rpackagemanager/rcmd"
 	"github.com/sirupsen/logrus"
@@ -12,29 +11,18 @@ import (
 func main() {
 	// ia := rcmd.InstallArgs{Library: "some/path"}
 	ia := rcmd.NewDefaultInstallArgs()
-	tmpdir := os.TempDir()
-	fmt.Println("tmpdir: ", tmpdir)
-	ia.Library = tmpdir
-	ib := &rcmd.InstallArgs{
-		Library: "../integration_tests/lib",
-	}
-	fmt.Println(ia.CliArgs())
+	ia.Library = "../integration_tests/lib"
 	appFS := afero.NewOsFs()
 	lg := logrus.New()
 
 	lg.Level = logrus.DebugLevel
-	res, err := rcmd.Install(appFS,
+	res, err := rcmd.InstallBinary(appFS,
 		"../integration_tests/src/test1_0.0.1.tar.gz",
 		ia,
 		rcmd.RSettings{},
 		rcmd.ExecSettings{},
 		lg)
-	res, err = rcmd.Install(appFS,
-		"test1_0.0.1.tgz",
-		ib,
-		rcmd.RSettings{},
-		rcmd.ExecSettings{},
-		lg)
+
 	fmt.Println("res: ", res)
 	fmt.Println("err: ", err)
 }
