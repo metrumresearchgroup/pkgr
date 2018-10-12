a2ef0f88149f0e2e530c36185b55953c4d0f07fe
Devin Pastoor <devin.pastoor@gmail.com>
2018-10-12 00:07:02 -0400

    package version handling

---
+++ desc/version.go
@@ -0,0 +1,91 @@
+package desc
+
+import (
+	"regexp"
+	"strconv"
+)
+
+// ParseVersion parses a package string to a version
+func ParseVersion(v string) Version {
+	ver := Version{}
+	parts := regexp.MustCompile(`[\.-]`).Split(v, 4)
+	ver.Major, _ = strconv.Atoi(parts[0])
+	ver.Minor, _ = strconv.Atoi(parts[1])
+	if len(parts) > 2 {
+		ver.Patch, _ = strconv.Atoi(parts[2])
+	}
+	if len(parts) > 3 {
+		ver.Dev, _ = strconv.Atoi(parts[3])
+
+	}
+	if len(parts) > 4 {
+		ver.Other, _ = strconv.Atoi(parts[4])
+
+	}
+	return ver
+}
+
+// CompareVersionStrings compares version strings
+func CompareVersionStrings(v1 string, v2 string) int {
+	return CompareVersions(ParseVersion(v1), ParseVersion(v2))
+}
+
+// CompareVersions compares two version numbers
+// similar to bytes.Compare, where
+// if v1 < v2 returns - 1, equal 0, v1 > v2 1
+func CompareVersions(v1 Version, v2 Version) int {
+	switch {
+	case v1.Major < v2.Major:
+		return -1
+	case v1.Major > v2.Major:
+		return 1
+	}
+	switch {
+	case v1.Minor < v2.Minor:
+		return -1
+	case v1.Minor > v2.Minor:
+		return 1
+	}
+	switch {
+	case v1.Patch < v2.Patch:
+		return -1
+	case v1.Patch > v2.Patch:
+		return 1
+	}
+	switch {
+	case v1.Dev < v2.Dev:
+		return -1
+	case v1.Dev > v2.Dev:
+		return 1
+	}
+	switch {
+	case v1.Other < v2.Other:
+		return -1
+	case v1.Other > v2.Other:
+		return 1
+	}
+
+	return 0
+}
+
+// Versions represents an array of Version numbers
+type Versions []Version
+
+func isLowerVersion(v1 Version, v2 Version) bool {
+	if v1.Major < v2.Major {
+		return true
+	}
+	if v1.Minor < v2.Minor {
+		return true
+	}
+	if v1.Dev < v2.Dev {
+		return true
+	}
+	if v1.Other < v2.Other {
+		return true
+	}
+	return false
+}
+func (v Versions) Len() int           { return len(v) }
+func (v Versions) Swap(i, j int)      { v[i], v[j] = v[j], v[i] }
+func (v Versions) Less(i, j int) bool { return isLowerVersion(v[i], v[j]) }
---
+++ desc/version_test.go
@@ -0,0 +1,82 @@
+package desc
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestVersion(t *testing.T) {
+	assert := assert.New(t)
+	var data = []struct {
+		in       string
+		expected Version
+	}{
+		{
+			"1.0.0",
+			Version{1, 0, 0, 0, 0},
+		},
+		{
+			"1.0-0",
+			Version{1, 0, 0, 0, 0},
+		},
+		{
+			"0.1.2.9000",
+			Version{0, 1, 2, 9000, 0},
+		},
+		{
+			"1.2.3.4",
+			Version{1, 2, 3, 4, 0},
+		},
+		{
+			"1-2-3-4",
+			Version{1, 2, 3, 4, 0},
+		},
+	}
+	for i, tt := range data {
+		actual := ParseVersion(tt.in)
+		assert.Equal(tt.expected, actual, fmt.Sprintf("test num: %v", i+1))
+	}
+}
+
+func TestComparisonString(t *testing.T) {
+	assert := assert.New(t)
+	var data = []struct {
+		in       []string
+		expected int
+	}{
+		{
+			[]string{"0.1.0", "0.1.1"},
+			-1,
+		},
+		{
+			[]string{"0.1.2", "0.1.1"},
+			1,
+		},
+		{
+			[]string{"0.1.1", "0.1.1"},
+			0,
+		},
+		{
+			[]string{"1.0.0", "0.1.1"},
+			1,
+		},
+		{
+			[]string{"0.0.9", "0.1.0"},
+			-1,
+		},
+		{
+			[]string{"0.0.0.1", "0.0.0.0"},
+			1,
+		},
+		{
+			[]string{"0.0.0.0", "0.0.0.0"},
+			0,
+		},
+	}
+	for i, tt := range data {
+		actual := CompareVersionStrings(tt.in[0], tt.in[1])
+		assert.Equal(tt.expected, actual, fmt.Sprintf("test num: %v", i+1))
+	}
+}
