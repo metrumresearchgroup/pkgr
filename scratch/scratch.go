46e0e58010f2b47f1bb2c306ce8b82a7f752f094
Devin Pastoor <devin.pastoor@gmail.com>
2018-10-12 00:07:52 -0400

    add struct-based pretty printing

--- scratch/scratch.go
+++ scratch/scratch.go
@@ -1,29 +1,36 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 
-	"github.com/dpastoor/rpackagemanager/rcmd"
+	"github.com/dpastoor/rpackagemanager/desc"
+	"github.com/dpastoor/rpackagemanager/rpkg"
 	"github.com/sirupsen/logrus"
 	"github.com/spf13/afero"
 )
 
 func main() {
 	// ia := rcmd.InstallArgs{Library: "some/path"}
-	ia := rcmd.NewDefaultInstallArgs()
-	ia.Library = "../integration_tests/lib"
 	appFS := afero.NewOsFs()
 	lg := logrus.New()
-
 	lg.Level = logrus.DebugLevel
-	res, binaryPath, err := rcmd.InstallBinary(appFS,
-		"../integration_tests/src/test1_0.0.1.tar.gz",
-		ia,
-		rcmd.RSettings{},
-		rcmd.ExecSettings{},
-		lg)
 
-	fmt.Println("res: ", res)
-	fmt.Println("binary path: ", binaryPath)
-	fmt.Println("err: ", err)
+	// b28ba6e911e86ae4e682f834741e85e0
+	hash, _ := rpkg.Hash(appFS, "../integration_tests/src/test1_0.0.1.tar.gz")
+	fmt.Println(hash)
+
+	d, err := desc.ReadDesc("../desc/testdata/D2")
+	if err != nil {
+		panic(err)
+	}
+	PrettyPrint(d)
+}
+
+func PrettyPrint(v interface{}) (err error) {
+	b, err := json.MarshalIndent(v, "", "  ")
+	if err == nil {
+		fmt.Println(string(b))
+	}
+	return
 }
