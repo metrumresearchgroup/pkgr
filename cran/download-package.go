6379643f991e7830f3db7cbf34579fd135f78392
Devin Pastoor <devin.pastoor@gmail.com>
2018-10-28 19:30:55 -0400

    return a type with metadata about packages after downloading and change data structure
    map will allow rapid access and selection from previously solved layers.
    returning information about whether it was newly downloaded
    and the type can help downstream
    for determining the appropriate way to install, etc

--- cran/download-package.go
+++ cran/download-package.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"io"
 	"net/http"
+	"os"
 	"path/filepath"
 	"strings"
 	"sync"
@@ -13,9 +14,26 @@ import (
 	"github.com/spf13/afero"
 )
 
+// SourceType represents the type of package to download
+type SourceType int
+
+// Constraints on package deps
+// Least to most constraining
+const (
+	Source SourceType = iota
+	Binary
+)
+
+// Download provides information about the package download
+type Download struct {
+	Type SourceType
+	Path string
+	New  bool
+}
+
 // DownloadPackages downloads a set of packages concurrently
-func DownloadPackages(fs afero.Fs, ds []desc.Desc, url string, dir string) error {
-	// bound to
+func DownloadPackages(fs afero.Fs, ds []desc.Desc, url string, st SourceType, dir string) (map[string](Download), error) {
+	result := NewSyncMap()
 	sem := make(chan struct{}, 10)
 	wg := sync.WaitGroup{}
 	for _, d := range ds {
@@ -27,43 +45,62 @@ func DownloadPackages(fs afero.Fs, ds []desc.Desc, url string, dir string) error
 				wg.Done()
 			}()
 			pkgdir := filepath.Join(dir, fmt.Sprintf("%s_%s.tar.gz", d.Package, d.Version))
-			DownloadPackage(fs, d, url, pkgdir)
+			dl, err := DownloadPackage(fs, d, url, st, pkgdir)
+			if err != nil {
+				fmt.Println("downloading failed for package: ", d.Package)
+				return
+			}
+			result.Put(d.Package, dl)
 		}(d, &wg)
 	}
 	wg.Wait()
 	fmt.Println("all packages downloaded")
-	return nil
+	return result.Map, nil
 }
 
-func DownloadPackage(fs afero.Fs, d desc.Desc, url string, dest string) error {
-
-	ok, err := goutils.Exists(fs, dest)
-	if err != nil {
-		return err
+// DownloadPackage should download a package tarball if it doesn't exist and return
+// the path to the downloaded tarball
+func DownloadPackage(fs afero.Fs, d desc.Desc, url string, st SourceType, dest string) (Download, error) {
+	if !filepath.IsAbs(dest) {
+		cwd, _ := os.Getwd()
+		// turn to absolute
+		dest = filepath.Clean(filepath.Join(cwd, dest))
 	}
-	if ok {
+	exists, err := goutils.Exists(fs, dest)
+	if err != nil {
+		return Download{}, err
+	}
+	if exists {
 		fmt.Println("already have ", d.Package, " downloaded")
-		return nil
+		return Download{
+			Type: st,
+			Path: dest,
+			New:  false,
+		}, nil
 	}
 	pkgdl := fmt.Sprintf("%s/src/contrib/%s_%s.tar.gz", strings.TrimSuffix(url, "/"), d.Package, d.Version)
 	fmt.Println("downloading from: ", pkgdl)
 	resp, err := http.Get(pkgdl)
 	if err != nil {
 		fmt.Println("error downloading package", d.Package)
-		return err
+		return Download{}, err
 	}
 	defer resp.Body.Close()
 	file, err := fs.Create(dest)
 	if err != nil {
 		fmt.Println("couldn't create tarball for ", d.Package)
 		fmt.Println(err)
-		return err
+		return Download{}, err
 	}
 	defer file.Close()
 	_, err = io.Copy(file, resp.Body)
 	if err != nil {
-		return err
+		return Download{}, err
 	}
 
-	return nil
+	return Download{
+		Type: st,
+		Path: dest,
+		New:  true,
+	}, nil
 }
