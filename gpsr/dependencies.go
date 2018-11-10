0771a8eb55a7559d0583aa75cd2c4a6f29135c69
Devin Pastoor <devin.pastoor@gmail.com>
2018-11-10 10:25:34 -0500

    break out dep resolution to separate file for better org

--- gpsr/dependencies.go
+++ gpsr/dependencies.go
@@ -5,7 +5,6 @@ import (
 	"fmt"
 
 	"github.com/deckarep/golang-set"
-	"github.com/dpastoor/rpackagemanager/cran"
 )
 
 type Graph map[string]*Node
@@ -99,56 +98,6 @@ func ResolveLayers(graph Graph) ([][]string, error) {
 	}
 
 	return resolved, nil
-}
-
-// ResolveInstallationReqs resolves all the installation requirements
-func ResolveInstallationReqs(pkgs []string, pkgdb *cran.PkgDb) (InstallPlan, error) {
-	workingGraph := NewGraph()
-	depDb := make(map[string][]string)
-	for _, p := range pkgs {
-		pkg, _, _ := pkgdb.GetPackage(p)
-		appendToGraph(workingGraph, pkg, pkgdb)
-	}
-	resolved, err := ResolveLayers(workingGraph)
-	if err != nil {
-		fmt.Println("error resolving graph")
-		return InstallPlan{}, err
-	}
-	for i, l := range resolved {
-		if i == 0 {
-			// don't need to know dep tree for first layer as shouldn't have any deps
-			continue
-		}
-		for _, p := range l {
-			workingGraph := NewGraph()
-			pkg, _, _ := pkgdb.GetPackage(p)
-			appendToGraph(workingGraph, pkg, pkgdb)
-			resolved, err := ResolveLayers(workingGraph)
-			if err != nil {
-				fmt.Println("error resolving graph")
-				return InstallPlan{}, err
-			}
-			allDeps := resolved[0]
-			for j, rl := range resolved {
-				if j == 0 {
-					continue
-				}
-				if j+1 == len(resolved) {
-					// for last layer don't add the package itself
-					for _, pkg := range rl {
-						if pkg != p {
-							allDeps = append(allDeps, pkg)
-						}
-					}
-				} else {
-					allDeps = append(allDeps, rl...)
-				}
-			}
-			depDb[p] = allDeps
-		}
-	}
-	return InstallPlan{StartingPackages: resolved[0],
-		DepDb: depDb}, nil
 }
 
 // InvertDependencies provides an inversion of the dependencies
--- gpsr/solution.go
+++ gpsr/solution.go
@@ -1,1 +1,57 @@
 package gpsr
+
+import (
+	"fmt"
+
+	"github.com/dpastoor/rpackagemanager/cran"
+)
+
+// ResolveInstallationReqs resolves all the installation requirements
+func ResolveInstallationReqs(pkgs []string, pkgdb *cran.PkgDb) (InstallPlan, error) {
+	workingGraph := NewGraph()
+	depDb := make(map[string][]string)
+	for _, p := range pkgs {
+		pkg, _, _ := pkgdb.GetPackage(p)
+		appendToGraph(workingGraph, pkg, pkgdb)
+	}
+	resolved, err := ResolveLayers(workingGraph)
+	if err != nil {
+		fmt.Println("error resolving graph")
+		return InstallPlan{}, err
+	}
+	for i, l := range resolved {
+		if i == 0 {
+			// don't need to know dep tree for first layer as shouldn't have any deps
+			continue
+		}
+		for _, p := range l {
+			workingGraph := NewGraph()
+			pkg, _, _ := pkgdb.GetPackage(p)
+			appendToGraph(workingGraph, pkg, pkgdb)
+			resolved, err := ResolveLayers(workingGraph)
+			if err != nil {
+				fmt.Println("error resolving graph")
+				return InstallPlan{}, err
+			}
+			allDeps := resolved[0]
+			for j, rl := range resolved {
+				if j == 0 {
+					continue
+				}
+				if j+1 == len(resolved) {
+					// for last layer don't add the package itself
+					for _, pkg := range rl {
+						if pkg != p {
+							allDeps = append(allDeps, pkg)
+						}
+					}
+				} else {
+					allDeps = append(allDeps, rl...)
+				}
+			}
+			depDb[p] = allDeps
+		}
+	}
+	return InstallPlan{StartingPackages: resolved[0],
+		DepDb: depDb}, nil
+}
