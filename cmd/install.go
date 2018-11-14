0e00538b93d9fd3acda498ff1a25f68c86afde4d
Devin Pastoor <devin.pastoor@gmail.com>
2018-11-14 14:28:45 -0500

    move over scratch work to install command

--- cmd/install.go
+++ cmd/install.go
@@ -16,7 +16,14 @@ package cmd
 
 import (
 	"fmt"
+	"os"
+	"path/filepath"
+	"time"
 
+	"github.com/dpastoor/rpackagemanager/cran"
+	"github.com/dpastoor/rpackagemanager/gpsr"
+	"github.com/dpastoor/rpackagemanager/rcmd"
+	"github.com/sajari/fuzzy"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
 )
@@ -26,22 +33,112 @@ var installCmd = &cobra.Command{
 	Use:   "install",
 	Short: "install a package",
 	Long: `
-	JUST FOR EXPERIMENTATION
+	install a package
  `,
 	RunE: rInstall,
 }
 
 func rInstall(cmd *cobra.Command, args []string) error {
+	startTime := time.Now()
+	repos := []cran.RepoURL{
+		cran.RepoURL{
+			Name: "CRAN",
+			URL:  "https://cran.rstudio.com",
+		},
+		cran.RepoURL{
+			Name: "gh_releases",
+			URL:  "https://metrumresearchgroup.github.io/rpkgs/gh_releases",
+		},
+	}
+	cdb, err := cran.NewPkgDb(repos)
+	if err != nil {
+		fmt.Println("error getting pkgdb ", err)
+		panic(err)
+	}
+	//PrettyPrint(cdb)
+	for _, db := range cdb.Db {
+		fmt.Println(fmt.Sprintf("%v packages available in for %s from %s", len(db.Db), db.Repo.Name, db.Repo.URL))
+	}
 
-	//AppFs := afero.NewOsFs()
-	// can use this to redirect log output
-	// f, err := os.OpenFile("testlogfile", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
-	// if err != nil {
-	// 	log.Fatalf("error opening file: %v", err)
-	// }
-	// defer f.Close()
+	pkgs := []string{
+		// "PKPDmisc",
+		// "mrgsolve",
+		// "rmarkdown",
+		// "bitops",
+		// "caTools",
+		// "GGally",
+		// "knitr",
+		// "gridExtra",
+		// "htmltools",
+		// "xtable",
+		// "ggplot2",
+		//"dplyr",
+		// "shiny",
+		// "shinydashboard",
+		// "data.table",
+		// "logrr",
+		// "crayon",
+		// "glue",
+		// "rcpp",
 
-	fmt.Println("library: ", viper.GetString("library"))
+		// should cover misspelled packages!!
+		//"tidyVerse",
+	}
+	ap := cdb.GetPackages(pkgs)
+	if len(ap.Missing) > 0 {
+		fmt.Println("missing packages: ", ap.Missing)
+		model := fuzzy.NewModel()
+
+		// For testing only, this is not advisable on production
+		model.SetThreshold(1)
+
+		// This expands the distance searched, but costs more resources (memory and time).
+		// For spell checking, "2" is typically enough, for query suggestions this can be higher
+		model.SetDepth(1)
+		pkgs := cdb.GetAllPkgsByName()
+		model.Train(pkgs)
+		for _, mp := range ap.Missing {
+			fmt.Println("did you mean one of: ", model.Suggestions(mp, false))
+		}
+		os.Exit(1)
+	}
+	ip, err := gpsr.ResolveInstallationReqs(pkgs, cdb)
+	if err != nil {
+		fmt.Println(err)
+		panic(err)
+	}
+	fmt.Println(time.Since(startTime))
+	if err != nil {
+		log.Fatalf("Failed to resolve dependency graph: %s\n", err)
+	} else {
+		log.Info("The dependency graph resolved successfully")
+	}
+	var toDl []cran.PkgDl
+	// starting packages
+	for _, p := range ip.StartingPackages {
+		pkg, repo, _ := cdb.GetPackage(p)
+		toDl = append(toDl, cran.PkgDl{Package: pkg, Repo: repo})
+	}
+	// all other packages
+	for p := range ip.DepDb {
+		pkg, repo, _ := cdb.GetPackage(p)
+		toDl = append(toDl, cran.PkgDl{Package: pkg, Repo: repo})
+	}
+	// // want to download the packages and return the full path of any downloaded package
+	dl, err := cran.DownloadPackages(fs, toDl, cran.Source, "dump/cache")
+	if err != nil {
+		fmt.Println(err)
+		panic(err)
+	}
+
+	ia := rcmd.NewDefaultInstallArgs()
+	ia.Library, _ = filepath.Abs("/Users/devinp/repos/queue-cancellation/packrat/lib/x86_64-apple-darwin15.6.0/3.5.1")
+	err = rcmd.InstallPackagePlan(fs, ip, dl, rcmd.NewPackageCache("dump/cache", false), ia, rcmd.NewRSettings(), rcmd.ExecSettings{}, log, 12)
+	if err != nil {
+		fmt.Println("failed package install")
+		fmt.Println(err)
+	}
+	fmt.Println("duration:", time.Since(startTime))
 	return nil
 }
 
--- scratch/scratch.go
+++ scratch/scratch.go
@@ -3,15 +3,7 @@ package main
 import (
 	"encoding/json"
 	"fmt"
-	"os"
-	"path/filepath"
-	"time"
 
-	"github.com/sajari/fuzzy"
-
-	"github.com/dpastoor/rpackagemanager/cran"
-	"github.com/dpastoor/rpackagemanager/gpsr"
-	"github.com/dpastoor/rpackagemanager/rcmd"
 	"github.com/sirupsen/logrus"
 	"github.com/spf13/afero"
 )
@@ -24,126 +16,7 @@ func main() {
 	// appFS.Remove("logfile.txt")
 	// logf, _ := appFS.OpenFile("logfile.txt", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
 	// log.SetOutput(logf)
-	startTime := time.Now()
-	repos := []cran.RepoURL{
-		cran.RepoURL{
-			Name: "CRAN",
-			URL:  "https://cran.rstudio.com",
-		},
-		cran.RepoURL{
-			Name: "gh_releases",
-			URL:  "https://metrumresearchgroup.github.io/rpkgs/gh_releases",
-		},
-	}
-	cdb, err := cran.NewPkgDb(repos)
-	if err != nil {
-		fmt.Println("error getting pkgdb ", err)
-		panic(err)
-	}
-	//PrettyPrint(cdb)
-	for _, db := range cdb.Db {
-		fmt.Println(fmt.Sprintf("%v packages pulled in for %s from %s", len(db.Db), db.Repo.Name, db.Repo.URL))
-	}
 
-	p, repo, _ := cdb.GetPackage("logrrr")
-	fmt.Println(repo)
-	PrettyPrint(p)
-
-	p, repo, _ = cdb.GetPackage("PKPDmisc")
-	fmt.Println(repo)
-	PrettyPrint(p)
-	// PrettyPrint(dmap["dplyr"])
-	// PrettyPrint(dmap["PKPDmisc"])
-	//AppFs := afero.NewOsFs()
-	// can use this to redirect log output
-	// f, err := os.OpenFile("testlogfile", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
-	// if err != nil {
-	// 	log.Fatalf("error opening file: %v", err)
-	// }
-	// defer f.Close()
-	pkgs := []string{
-		"PKPDmisc",
-		"mrgsolve",
-		"rmarkdown",
-		"bitops",
-		"caTools",
-		"GGally",
-		"knitr",
-		"gridExtra",
-		"htmltools",
-		"xtable",
-		"ggplot2",
-		"dplyr",
-		"shiny",
-		"shinydashboard",
-		"data.table",
-		"logrrr",
-		"crayon",
-		"glue",
-		"rcpp",
-
-		// should cover misspelled packages!!
-		//"tidyVerse",
-	}
-	ap := cdb.GetPackages(pkgs)
-	if len(ap.Missing) > 0 {
-		fmt.Println("missing packages: ", ap.Missing)
-		model := fuzzy.NewModel()
-
-		// For testing only, this is not advisable on production
-		model.SetThreshold(1)
-
-		// This expands the distance searched, but costs more resources (memory and time).
-		// For spell checking, "2" is typically enough, for query suggestions this can be higher
-		model.SetDepth(1)
-		pkgs := cdb.GetAllPkgsByName()
-		model.Train(pkgs)
-		for _, mp := range ap.Missing {
-			fmt.Println("did you mean one of: ", model.Suggestions(mp, false))
-		}
-		os.Exit(1)
-	}
-	ip, err := gpsr.ResolveInstallationReqs(pkgs, cdb)
-	if err != nil {
-		fmt.Println(err)
-		panic(err)
-	}
-	fmt.Println(time.Since(startTime))
-	PrettyPrint(ip)
-	fmt.Println("inverted dependencies: ")
-	PrettyPrint(ip.InvertDependencies())
-	if err != nil {
-		log.Fatalf("Failed to resolve dependency graph: %s\n", err)
-	} else {
-		log.Info("The dependency graph resolved successfully")
-	}
-	var toDl []cran.PkgDl
-	// starting packages
-	for _, p := range ip.StartingPackages {
-		pkg, repo, _ := cdb.GetPackage(p)
-		toDl = append(toDl, cran.PkgDl{Package: pkg, Repo: repo})
-	}
-	// all other packages
-	for p := range ip.DepDb {
-		pkg, repo, _ := cdb.GetPackage(p)
-		toDl = append(toDl, cran.PkgDl{Package: pkg, Repo: repo})
-	}
-	// // want to download the packages and return the full path of any downloaded package
-	dl, err := cran.DownloadPackages(appFS, toDl, cran.Source, "dump/cache")
-	if err != nil {
-		fmt.Println(err)
-		panic(err)
-	}
-
-	ia := rcmd.NewDefaultInstallArgs()
-	ia.Library, _ = filepath.Abs("dump/test5lib/")
-	fmt.Println("library set to: ", ia.Library)
-	err = rcmd.InstallPackagePlan(appFS, ip, dl, rcmd.NewPackageCache("dump/cache", false), ia, rcmd.NewRSettings(), rcmd.ExecSettings{}, log, 12)
-	if err != nil {
-		fmt.Println("failed package install")
-		fmt.Println(err)
-	}
-	fmt.Println("duration:", time.Since(startTime))
 	// fmt.Println("library: ", viper.GetString("library"))
 }
 func PrettyPrint(v interface{}) (err error) {
