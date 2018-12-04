4157bb7ac60127edc6058631915701ac4cb220cf
Devin Pastoor <devin.pastoor@gmail.com>
2018-12-04 18:26:02 -0500

    fix: wrong split causing env vars to be dropped
    SplitN(..., 1) meant to return only len(1), actually needed splitN(...,2)
    to get a key, value back to create the env map

--- rcmd/install.go
+++ rcmd/install.go
@@ -67,7 +67,7 @@ func configureEnv(
 	envVars := os.Environ()
 	envMap := make(map[string]string)
 	for _, ev := range envVars {
-		evs := strings.SplitN(ev, "=", 1)
+		evs := strings.SplitN(ev, "=", 2)
 		if len(evs) > 1 && evs[1] != "" {
 			envMap[evs[0]] = evs[1] 
 		}
