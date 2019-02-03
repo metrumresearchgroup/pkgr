dc40fb6f3ce837d4e9c41a90d4e948b8683ce922
Devin Pastoor <devin.pastoor@gmail.com>
2019-02-03 13:26:16 -0500

    refactor: simplify configureEnv and make more deterministic
    Now, among other changes,
     just ignore R_LIBS_USER so don't need to worry about overriding.
    
    references: #34

--- rcmd/configure.go
+++ rcmd/configure.go
@@ -8,39 +8,49 @@ import (
 	log "github.com/sirupsen/logrus"
 )
 
-// envVars contains the default environment variables usually from
+// sysEnvVars contains the default environment variables usually from
 // os.Environ()
-func configureEnv(envVars []string, rs RSettings, pkg string) []string {
+func configureEnv(sysEnvVars []string, rs RSettings, pkg string) []string {
 	envMap := make(map[string]string)
-	for _, ev := range envVars {
+	envVars := []string{}
+	envOrder := []string{}
+
+	// system env vars generally
+	for _, ev := range sysEnvVars {
 		evs := strings.SplitN(ev, "=", 2)
 		if len(evs) > 1 && evs[1] != "" {
+
+			// we don't want to track the order of these anyway since they should take priority in the end
+			// R_LIBS_USER takes precidence over R_LIBS_SITE
+			// so will cause the loading characteristics to
+			// not be representative of the hierarchy specified
+			// in Library/Libpaths in the pkgr configuration
+			// we only want R_LIBS_SITE set to control all relevant library paths for the user to
+			if evs[0] == "R_LIBS_USER" {
+				log.WithField("path", evs[1]).Debug("deleting set R_LIBS_USER")
+				continue
+			}
+			if evs[0] == "R_LIBS_SITE" {
+				log.WithField("path", evs[1]).Debug("deleting set R_LIBS_USER")
+				continue
+			}
 			envMap[evs[0]] = evs[1]
+			envOrder = append(envOrder, evs[0])
 		}
 	}
-	rlu, exists := envMap["R_LIBS_USER"]
-	if exists {
-		// R_LIBS_USER takes precidence over R_LIBS_SITE
-		// so will cause the loading characteristics to
-		// not be representative of the hierarchy specified
-		// in Library/Libpaths in the pkgr configuration
-		delete(envMap, "R_LIBS_USER")
-		log.WithField("path", rlu).Debug("deleting set R_LIBS_USER")
-	}
-	envVars = []string{}
+
+	// TODO: determine if using globalenvvars as a map could cause subtle bug given ordering precedence
 	for k, v := range rs.GlobalEnvVars {
 		envMap[k] = v
 	}
 
 	ok, lp := rs.LibPathsEnv()
 	if ok {
-		// if LibPaths set, lets drop R_LIBS_SITE set as an ENV and instead
-		// add the generated R_LIBS_SITE from LibPathsEnv
-		delete(envMap, "R_LIBS_SITE")
 		envVars = append(envVars, lp)
 	}
-	for k, v := range envMap {
-		envVars = append(envVars, fmt.Sprintf("%s=%s", k, v))
+	for _, ev := range envOrder {
+		val := envMap[ev]
+		envVars = append(envVars, fmt.Sprintf("%s=%s", ev, val))
 	}
 	pkgEnv, hasCustomEnv := rs.PkgEnvVars[pkg]
 	if hasCustomEnv {
@@ -56,9 +66,6 @@ func configureEnv(envVars []string, rs RSettings, pkg string) []string {
 			"package": pkg,
 		}).Trace("Custom Environment Variables")
 	}
-	// double down on overwriting any specification of user customization
-	// and set R_LIBS_SITE to the same as the user
-	envVars = append(envVars, strings.Replace(lp, "R_LIBS_SITE", "R_LIBS_USER", 1))
 
 	return envVars
 }
--- rcmd/configure_test.go
+++ rcmd/configure_test.go
@@ -25,60 +25,60 @@ func TestConfigureArgs(t *testing.T) {
 			"minimal",
 			"",
 			[]string{},
-			[]string{"R_LIBS_SITE=path/to/install/lib", "R_LIBS_USER=path/to/install/lib"},
+			[]string{"R_LIBS_SITE=path/to/install/lib"},
 		},
 		{
 			"non-impactful system env set",
 			"",
 			[]string{"MISC_ENV=foo", "MISC2=bar"},
-			[]string{"R_LIBS_SITE=path/to/install/lib", "MISC2=bar", "MISC_ENV=foo", "R_LIBS_USER=path/to/install/lib"},
+			[]string{"R_LIBS_SITE=path/to/install/lib", "MISC_ENV=foo", "MISC2=bar"},
 		},
 		{
 			"non-impactful system env set with known package",
 			"dplyr",
 			[]string{"MISC_ENV=foo", "MISC2=bar"},
-			[]string{"R_LIBS_SITE=path/to/install/lib", "MISC2=bar", "MISC_ENV=foo", "R_LIBS_USER=path/to/install/lib"},
+			[]string{"R_LIBS_SITE=path/to/install/lib", "MISC_ENV=foo", "MISC2=bar"},
 		},
 		{
 			"R_LIBS_SITE env set",
 			"",
 			[]string{"R_LIBS_SITE=original/path", "MISC2=bar"},
-			[]string{"R_LIBS_SITE=path/to/install/lib", "MISC2=bar", "R_LIBS_USER=path/to/install/lib"},
+			[]string{"R_LIBS_SITE=path/to/install/lib", "MISC2=bar"},
 		},
 		{
 			"R_LIBS_SITE env set with known package",
 			"dplyr",
 			[]string{"R_LIBS_SITE=original/path", "MISC2=bar"},
-			[]string{"R_LIBS_SITE=path/to/install/lib", "MISC2=bar", "R_LIBS_USER=path/to/install/lib"},
+			[]string{"R_LIBS_SITE=path/to/install/lib", "MISC2=bar"},
 		},
 		{
 			"R_LIBS_USER env set",
 			"",
 			[]string{"R_LIBS_USER=original/path", "MISC2=bar"},
-			[]string{"R_LIBS_SITE=path/to/install/lib", "MISC2=bar", "R_LIBS_USER=path/to/install/lib"},
+			[]string{"R_LIBS_SITE=path/to/install/lib", "MISC2=bar"},
 		},
 		{
 			"R_LIBS_USER env set with known package",
 			"dplyr",
 			[]string{"R_LIBS_USER=original/path", "MISC2=bar"},
-			[]string{"R_LIBS_SITE=path/to/install/lib", "MISC2=bar", "R_LIBS_USER=path/to/install/lib"},
+			[]string{"R_LIBS_SITE=path/to/install/lib", "MISC2=bar"},
 		},
 		{
 			"R_LIBS_SITE and R_LIBS_USER env set",
 			"",
 			[]string{"R_LIBS_USER=original/path", "R_LIBS_SITE=original/site/path", "MISC2=bar"},
-			[]string{"R_LIBS_SITE=path/to/install/lib", "MISC2=bar", "R_LIBS_USER=path/to/install/lib"},
+			[]string{"R_LIBS_SITE=path/to/install/lib", "MISC2=bar"},
 		},
 		{
 			"R_LIBS_SITE and R_LIBS_USER env set",
 			"dplyr",
 			[]string{"R_LIBS_USER=original/path", "R_LIBS_SITE=original/site/path", "MISC2=bar"},
-			[]string{"R_LIBS_SITE=path/to/install/lib", "MISC2=bar", "R_LIBS_USER=path/to/install/lib"},
+			[]string{"R_LIBS_SITE=path/to/install/lib", "MISC2=bar"},
 		},
 	}
 	for i, tt := range installArgsTests {
 		actual := configureEnv(tt.sysEnv, defaultRS, tt.in)
-		assert.Equal(actual, tt.expected, fmt.Sprintf("%s, test num: %v", tt.context, i+1))
+		assert.Equal(tt.expected, actual, fmt.Sprintf("%s, test num: %v", tt.context, i+1))
 	}
 
 }
