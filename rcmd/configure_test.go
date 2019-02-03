3fb7aec27ec9634380a165680f8b536ec360f396
Devin Pastoor <devin.pastoor@gmail.com>
2019-02-02 19:32:47 -0500

    test: configureEnv tests, currently non-deterministically fail due to env map
    as maps are not sorted the key order is different so system  environment variables
    and multiple user defined variables will come out in a non-deterministic order
    
    references: #34

--- rcmd/configure_test.go
+++ rcmd/configure_test.go
@@ -10,24 +10,75 @@ import (
 func TestConfigureArgs(t *testing.T) {
 	assert := assert.New(t)
 
+	defaultRS := NewRSettings()
+	// there should always be at least one libpath
+	defaultRS.LibPaths = []string{"path/to/install/lib"}
+
 	var installArgsTests = []struct {
-		in       string
+		context string
+		in      string
+		// mocked system environment variables per os.Environ()
+		sysEnv   []string
 		expected []string
 	}{
 		{
+			"minimal",
 			"",
+			[]string{},
 			[]string{"R_LIBS_SITE=path/to/install/lib", "R_LIBS_USER=path/to/install/lib"},
 		},
 		{
+			"non-impactful system env set",
+			"",
+			[]string{"MISC_ENV=foo", "MISC2=bar"},
+			[]string{"R_LIBS_SITE=path/to/install/lib", "MISC2=bar", "MISC_ENV=foo", "R_LIBS_USER=path/to/install/lib"},
+		},
+		{
+			"non-impactful system env set with known package",
 			"dplyr",
-			[]string{"R_LIBS_SITE=path/to/install/lib", "R_LIBS_USER=path/to/install/lib"},
+			[]string{"MISC_ENV=foo", "MISC2=bar"},
+			[]string{"R_LIBS_SITE=path/to/install/lib", "MISC2=bar", "MISC_ENV=foo", "R_LIBS_USER=path/to/install/lib"},
+		},
+		{
+			"R_LIBS_SITE env set",
+			"",
+			[]string{"R_LIBS_SITE=original/path", "MISC2=bar"},
+			[]string{"R_LIBS_SITE=path/to/install/lib", "MISC2=bar", "R_LIBS_USER=path/to/install/lib"},
+		},
+		{
+			"R_LIBS_SITE env set with known package",
+			"dplyr",
+			[]string{"R_LIBS_SITE=original/path", "MISC2=bar"},
+			[]string{"R_LIBS_SITE=path/to/install/lib", "MISC2=bar", "R_LIBS_USER=path/to/install/lib"},
+		},
+		{
+			"R_LIBS_USER env set",
+			"",
+			[]string{"R_LIBS_USER=original/path", "MISC2=bar"},
+			[]string{"R_LIBS_SITE=path/to/install/lib", "MISC2=bar", "R_LIBS_USER=path/to/install/lib"},
+		},
+		{
+			"R_LIBS_USER env set with known package",
+			"dplyr",
+			[]string{"R_LIBS_USER=original/path", "MISC2=bar"},
+			[]string{"R_LIBS_SITE=path/to/install/lib", "MISC2=bar", "R_LIBS_USER=path/to/install/lib"},
+		},
+		{
+			"R_LIBS_SITE and R_LIBS_USER env set",
+			"",
+			[]string{"R_LIBS_USER=original/path", "R_LIBS_SITE=original/site/path", "MISC2=bar"},
+			[]string{"R_LIBS_SITE=path/to/install/lib", "MISC2=bar", "R_LIBS_USER=path/to/install/lib"},
+		},
+		{
+			"R_LIBS_SITE and R_LIBS_USER env set",
+			"dplyr",
+			[]string{"R_LIBS_USER=original/path", "R_LIBS_SITE=original/site/path", "MISC2=bar"},
+			[]string{"R_LIBS_SITE=path/to/install/lib", "MISC2=bar", "R_LIBS_USER=path/to/install/lib"},
 		},
 	}
-	defaultRS := RSettings{}
-	defaultRS.LibPaths = []string{"path/to/install/lib"}
 	for i, tt := range installArgsTests {
-		actual := configureEnv([]string{}, defaultRS, tt.in)
-		assert.Equal(actual, tt.expected, fmt.Sprintf("test num: %v", i+1))
-
+		actual := configureEnv(tt.sysEnv, defaultRS, tt.in)
+		assert.Equal(actual, tt.expected, fmt.Sprintf("%s, test num: %v", tt.context, i+1))
 	}
+
 }
