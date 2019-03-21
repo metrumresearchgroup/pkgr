31937105b4fdd089a6c02ffa9ebcb79148e60fbb
John Carlo Salter <juuncaerlum@gmail.com>
2019-03-21 17:01:06 -0400

    chore: cleaning up some of the integration test directories.
    wip for issue #52

--- integration_tests/run_multiple_installs.sh
+++ integration_tests/run_multiple_installs.sh
@@ -1,8 +1,20 @@
 #!/bin/bash
 
+rm -rf ./testsets/simple
+mkdir ./testsets/simple
 pkgr install --config pkgr-simple.yml
+
+rm -rf ./testsets/simple-suggests
+mkdir ./testsets/simple-suggests
 pkgr install --config pkgr-simple-suggests.yml
-pkgr install --config mixed-source.
+
+rm -rf ./testsets/master
+mkdir ./testsets/master
+pkgr install --config master-pkgr.yml
+
+rm -rf ./testsets/internal
+mkdir ./testsets/internal
+pkgr install --config pkgr-internal.yml
 
 rm -rf ./testsets/multirepo/
 mkdir ./testsets/multirepo
