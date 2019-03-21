2eafae41b7470c75fc3dcbc13f40801247e09c9b
John Carlo Salter <juuncaerlum@gmail.com>
2019-03-21 10:52:25 -0400

    wip: Skeleton for clean command and arguments.

--- cmd/clean.go
+++ cmd/clean.go
@@ -16,19 +16,22 @@ package cmd
 
 import (
 	"fmt"
-	"strconv"
 
 	"github.com/spf13/cobra"
 )
 
 var cleanAll bool
-var cleanPkgdbs string
+var cleanPkgdbs bool
+var pkgdbs string
+var cleanCache bool
+var srcCaches string
+var binaryCaches string
 
 // cleanCmd represents the clean command
 var cleanCmd = &cobra.Command{
 	Use:   "clean",
 	Short: "clean up cached information",
-	Long:  "clean up cached source files and binaries, as well as the cached package database.",
+	Long:  "clean up cached source files and binaries, as well as the saved package database.",
 	RunE:  clean,
 	//	Run: func(cmd *cobra.Command, args []string) {
 	//		fmt.Println("clean called")
@@ -45,15 +48,51 @@ func init() {
 	// Cobra supports local flags which will only run when this command
 	// is called directly, e.g.:
 	// cleanCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
-	cleanCmd.Flags().Bool("all", true, "clean all cached items")
-	cleanCmd.Flags().StringVar(&cleanPkgdbs, "pkgdb", "ALL", "Remove cached package databases.")
+	cleanCmd.Flags().BoolVar(&cleanAll, "all", false, "clean all cached items")
+	cleanCmd.Flags().BoolVar(&cleanPkgdbs, "pkgdbs", false, "Remove cached package databases.")
+	cleanCmd.Flags().StringVar(&pkgdbs, "dbs", "ALL", "Package databases to remove.")
+	cleanCmd.Flags().BoolVar(&cleanCache, "cache", false, "Remove cache sources and/or binaries")
+	cleanCmd.Flags().StringVar(&srcCaches, "src", "ALL", "Clean src caches in clean --cache")
+	cleanCmd.Flags().StringVar(&binaryCaches, "binary", "ALL", "Clean binary caches in clean --cache")
 	//viper.BindFlag("pkgdb")
 
 	RootCmd.AddCommand(cleanCmd)
 }
 
 func clean(cmd *cobra.Command, args []string) error {
-	fmt.Println(fmt.Sprintf("cleanPkgdbs: %s", cleanPkgdbs))
-	fmt.Println(fmt.Sprintf("cleanAll: %s", strconv.FormatBool(cleanAll)))
+
+	if !cleanAll && !cleanPkgdbs && !cleanCache {
+		fmt.Println("No clean options passed -- not cleaning.")
+	}
+
+	if cleanAll {
+		fmt.Println("Cleaning all.")
+	} else {
+		if cleanPkgdbs {
+			if pkgdbs == "ALL" {
+				fmt.Println("Cleaning all pkgdbs")
+			} else {
+				fmt.Println(fmt.Sprintf("Cleaning specific package databases: %s", pkgdbs))
+			}
+
+			if cleanCache {
+				if srcCaches == "ALL" {
+					fmt.Println("Cleaning all src caches.")
+				} else {
+					fmt.Println(fmt.Sprintf("Cleaning specific src caches: %s", srcCaches))
+				}
+
+				if binaryCaches == "ALL" {
+					fmt.Println("Cleaning all binary caches.")
+				} else {
+					fmt.Println(fmt.Sprintf("Cleaning specific binary caches: %s", binaryCaches))
+				}
+			}
+		}
+	}
+	fmt.Println("Done.")
 	return nil
 }
+
+//When you bind a bool flag, it's basically "if that flag is not set then it's false, if it is set then it's true"
+//Default string is going
