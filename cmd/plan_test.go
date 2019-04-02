e41738241bc8622e4a0b81b44d27322606be15c5
John Carlo Salter <juuncaerlum@gmail.com>
2019-04-02 18:45:12 -0400

    add: null test

--- cmd/plan_test.go
+++ cmd/plan_test.go
@@ -35,40 +35,6 @@ func DestroyTestEnvironment() {
 }
 
 
-
-
-
-
-
-func TestGetPriorInstalledPackages_BasicTest (t *testing.T) {
-	fileSystem := InitializeTestEnvironment("basic-test1", "basic-test1")
-
-	cwd, _ := filepath.Abs(".")
-	fmt.Println(fmt.Sprintf("Starting test with working directory %s", cwd ))
-
-	expectedResult1 := InstalledPackage {
-		Name: "crayon",
-		Version: "1.3.4",
-		Repo: "CRAN",
-	}
-
-	expectedResult2 := InstalledPackage {
-		Name: "R6",
-		Version: "2.4.0",
-		Repo: "CRAN",
-	}
-
-	libraryPath, _ := filepath.Abs(filepath.Join("testsite", "working", "basic-test1", "test-library"))
-
-	actual := GetPriorInstalledPackages(fileSystem, libraryPath)
-
-	assert.Equal(t, 2, len(actual))
-	assert.True(t, installedPackagesAreEqual(expectedResult1, actual[expectedResult1.Name]))
-	assert.True(t, installedPackagesAreEqual(expectedResult2, actual[expectedResult2.Name]))
-
-	DestroyTestEnvironment()
-}
-
 func installedPackagesAreEqual(expected, actual InstalledPackage) bool {
 	return expected.Name == actual.Name && expected.Version == actual.Version && expected.Repo == actual.Repo
 }
@@ -102,4 +68,49 @@ func CopyDir(fs afero.Fs, src string, dst string) (string, error) {
 	}
 
 	return "Created " + dst + " from " + src, nil
+}
+
+func TestGetPriorInstalledPackages_BasicTest (t *testing.T) {
+	fileSystem := InitializeTestEnvironment("basic-test1", "basic-test1")
+
+	cwd, _ := filepath.Abs(".")
+	fmt.Println(fmt.Sprintf("Starting test with working directory %s", cwd ))
+
+	expectedResult1 := InstalledPackage {
+		Name: "crayon",
+		Version: "1.3.4",
+		Repo: "CRAN",
+	}
+
+	expectedResult2 := InstalledPackage {
+		Name: "R6",
+		Version: "2.4.0",
+		Repo: "CRAN",
+	}
+
+	libraryPath, _ := filepath.Abs(filepath.Join("testsite", "working", "basic-test1", "test-library"))
+
+	actual := GetPriorInstalledPackages(fileSystem, libraryPath)
+
+	assert.Equal(t, 2, len(actual))
+	assert.True(t, installedPackagesAreEqual(expectedResult1, actual[expectedResult1.Name]))
+	assert.True(t, installedPackagesAreEqual(expectedResult2, actual[expectedResult2.Name]))
+
+	DestroyTestEnvironment()
+}
+
+func TestGetPriorInstalledPackages_NoPreinstalledPackages(t *testing.T) {
+	fileSystem := InitializeTestEnvironment("basic-test1", "basic-test1")
+
+	cwd, _ := filepath.Abs(".")
+	fmt.Println(fmt.Sprintf("Starting test with working directory %s", cwd ))
+
+
+	libraryPath, _ := filepath.Abs(filepath.Join("testsite", "working", "null-test", "test-library"))
+
+	actual := GetPriorInstalledPackages(fileSystem, libraryPath)
+
+	assert.Equal(t, 0, len(actual))
+
+	DestroyTestEnvironment()
 }
