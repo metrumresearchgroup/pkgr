dc5d16dd15603caecc140abe0f7c7dfe8aa49ea3
John Carlo Salter <juuncaerlum@gmail.com>
2019-04-08 12:54:46 -0400

    change: changing unit tests to use testify Suites and related functions.

--- cmd/plan_test.go
+++ cmd/plan_test.go
@@ -4,18 +4,34 @@ import (
 	"fmt"
 	"github.com/dpastoor/goutils"
 	"github.com/spf13/afero"
-	//"os"
-	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/suite"
+
 	"path/filepath"
 	"testing"
 )
 
-func InitializeTestEnvironment(goldenSet, testName string) afero.Fs{
+type PlanTestSuite struct {
+	suite.Suite
+	FileSystem afero.Fs
+}
+
+func (suite *PlanTestSuite) SetupTest() {
+	suite.FileSystem = afero.NewOsFs()
+}
+
+func (suite *PlanTestSuite) TearDownTest() {
+	mm := afero.NewOsFs()
+	mm.RemoveAll("testsite/working")
+}
+
+
+func TestPlanTestSuite(t *testing.T) {
+	suite.Run(t, new(PlanTestSuite))
+}
+
+func InitializeTestEnvironment(fileSystem afero.Fs, goldenSet, testName string) {
 	goldenSetPath := filepath.Join("testsite", "golden", goldenSet)
 	testWorkDir := filepath.Join("testsite", "working", testName)
-
-	fileSystem := afero.NewOsFs()// afero.NewMemMapFs() //afero.NewOsFs()
-
 	fileSystem.MkdirAll(testWorkDir, 0755)
 
 	st, err := CopyDir(fileSystem, goldenSetPath, testWorkDir)
@@ -25,15 +41,56 @@ func InitializeTestEnvironment(goldenSet, testName string) afero.Fs{
 	} else {
 		fmt.Println(fmt.Sprintf("%s", st) )
 	}
-
-	return fileSystem
 }
 
-func DestroyTestEnvironment() {
-	mm := afero.NewOsFs()
-	mm.RemoveAll("testsite/working")
+func (suite *PlanTestSuite) TestGetPriorInstalledPackages_BasicTest () {
+	InitializeTestEnvironment(suite.FileSystem, "basic-test1", "basic-test1")
+
+
+	cwd, _ := filepath.Abs(".")
+	fmt.Println(fmt.Sprintf("Starting test with working directory %s", cwd ))
+
+	expectedResult1 := InstalledPackage {
+		Name: "crayon",
+		Version: "1.3.4",
+		Repo: "CRAN",
+	}
+
+	expectedResult2 := InstalledPackage {
+		Name: "R6",
+		Version: "2.4.0",
+		Repo: "CRAN",
+	}
+
+	libraryPath, _ := filepath.Abs(filepath.Join("testsite", "working", "basic-test1", "test-library"))
+
+	actual := GetPriorInstalledPackages(suite.FileSystem, libraryPath)
+
+	suite.Equal(2, len(actual))
+	suite.True(installedPackagesAreEqual(expectedResult1, actual[expectedResult1.Name]))
+	suite.True(installedPackagesAreEqual(expectedResult2, actual[expectedResult2.Name]))
+
+
 }
 
+func (suite *PlanTestSuite) TestGetPriorInstalledPackages_NoPreinstalledPackages() {
+	InitializeTestEnvironment(suite.FileSystem, "null-test", "null-test")
+
+	cwd, _ := filepath.Abs(".")
+	fmt.Println(fmt.Sprintf("Starting test with working directory %s", cwd ))
+
+
+	libraryPath, _ := filepath.Abs(filepath.Join("testsite", "working", "null-test", "test-library"))
+
+	actual := GetPriorInstalledPackages(suite.FileSystem, libraryPath)
+
+	suite.Equal(0, len(actual))
+}
+
+
+
+
+//////// Utility
 
 func installedPackagesAreEqual(expected, actual InstalledPackage) bool {
 	return expected.Name == actual.Name && expected.Version == actual.Version && expected.Repo == actual.Repo
@@ -69,49 +126,3 @@ func CopyDir(fs afero.Fs, src string, dst string) (string, error) {
 
 	return "Created " + dst + " from " + src, nil
 }
-
-func TestGetPriorInstalledPackages_BasicTest (t *testing.T) {
-	fileSystem := InitializeTestEnvironment("basic-test1", "basic-test1")
-
-	cwd, _ := filepath.Abs(".")
-	fmt.Println(fmt.Sprintf("Starting test with working directory %s", cwd ))
-
-	expectedResult1 := InstalledPackage {
-		Name: "crayon",
-		Version: "1.3.4",
-		Repo: "CRAN",
-	}
-
-	expectedResult2 := InstalledPackage {
-		Name: "R6",
-		Version: "2.4.0",
-		Repo: "CRAN",
-	}
-
-	libraryPath, _ := filepath.Abs(filepath.Join("testsite", "working", "basic-test1", "test-library"))
-
-	actual := GetPriorInstalledPackages(fileSystem, libraryPath)
-
-	assert.Equal(t, 2, len(actual))
-	assert.True(t, installedPackagesAreEqual(expectedResult1, actual[expectedResult1.Name]))
-	assert.True(t, installedPackagesAreEqual(expectedResult2, actual[expectedResult2.Name]))
-
-	DestroyTestEnvironment()
-}
-
-func TestGetPriorInstalledPackages_NoPreinstalledPackages(t *testing.T) {
-	fileSystem := InitializeTestEnvironment("null-test", "null-test")
-
-	cwd, _ := filepath.Abs(".")
-	fmt.Println(fmt.Sprintf("Starting test with working directory %s", cwd ))
-
-
-	libraryPath, _ := filepath.Abs(filepath.Join("testsite", "working", "null-test", "test-library"))
-
-	actual := GetPriorInstalledPackages(fileSystem, libraryPath)
-
-	assert.Equal(t, 0, len(actual))
-
-	DestroyTestEnvironment()
-}
-
