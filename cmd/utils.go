4ee79cd57b02cfec040ff6f8a55036cd5b9e63f5
John Carlo Salter <juuncaerlum@gmail.com>
2019-04-24 12:43:00 -0400

    chore: minorly tweaking log output.
    completes #75

--- cmd/utils.go
+++ cmd/utils.go
@@ -95,7 +95,7 @@ func scanInstalledPackage(
 	}
 	defer descriptionFile.Close()
 
-	log.WithField("description_file", descriptionFilePath).Debug("scanning DESCRIPTION file")
+	log.WithField("description_file", descriptionFilePath).Trace("scanning DESCRIPTION file")
 
 	installedPackage, err := desc.ParseDesc(descriptionFile)
 
