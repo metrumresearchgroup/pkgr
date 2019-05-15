73c3508c44cd6f1e7bdb1e3308ff686894ef064c
David <davidl@metrumrg.com>
2019-05-15 13:16:20 -0400

    feat: Store OS platform name in RSettings and use to construct file names

--- rcmd/Rsettings_test.go
+++ rcmd/Rsettings_test.go
@@ -1,7 +1,11 @@
 package rcmd
 
 import (
+	"fmt"
 	"testing"
+
+	"github.com/metrumresearchgroup/pkgr/cran"
+	"github.com/stretchr/testify/assert"
 )
 
 func TestLibPathsEnv(t *testing.T) {
@@ -34,5 +38,55 @@ func TestLibPathsEnv(t *testing.T) {
 		if actual != tt.expected {
 			t.Errorf("GOT: %s, WANT: %s", actual, tt.expected)
 		}
+	}
+}
+
+func TestGetRVersion(t *testing.T) {
+	var rVersionTests = []struct {
+		version    cran.RVersion
+		platform   string
+		message    string
+		assertTrue bool
+	}{
+		{
+			cran.RVersion{
+				Major: 3,
+				Minor: 5,
+				Patch: 3,
+			},
+			"x86_64-apple-darwin15.6.0",
+			"version info - accurate on mac",
+			true,
+		},
+		{
+			cran.RVersion{
+				Major: 9,
+				Minor: 9,
+				Patch: 9,
+			},
+			"999_x86_64-apple-darwin15.6.0",
+			"version info - wrong version",
+			false,
+		},
+	}
+	for _, tt := range rVersionTests {
+		rSettings := NewRSettings("")
+		version := GetRVersion(&rSettings)
+		if tt.assertTrue {
+			assert.Equal(t, tt.version, version, fmt.Sprintf("Not equal: %s", tt.message))
+			assert.Equal(t, tt.version, rSettings.Version, fmt.Sprintf("Not equal: %s", tt.message))
+			assert.Equal(t, tt.platform, rSettings.Platform, fmt.Sprintf("Not equal: %s", tt.message))
+		} else {
+			assert.NotEqual(t, tt.version, version, fmt.Sprintf("Equal: %s", tt.message))
+			assert.NotEqual(t, tt.version, rSettings.Version, fmt.Sprintf("Equal: %s", tt.message))
+			assert.NotEqual(t, tt.platform, rSettings.Platform, fmt.Sprintf("Equal: %s", tt.message))
+		}
+	}
+}
+
+func BenchmarkGetVersionInfo(b *testing.B) {
+	rSettings := NewRSettings("")
+	for n := 0; n < 10; n++ {
+		GetRVersion(&rSettings)
 	}
 }
