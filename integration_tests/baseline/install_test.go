643010c15190a39a8a103c26eb1f2c9e0d50aeaf
Devin Pastoor <devinp@metrumrg.com>
2021-07-04 15:58:06 -0400

    improve test structuring with makefile plus subtests

--- integration_tests/baseline/install_test.go
+++ integration_tests/baseline/install_test.go
@@ -17,18 +17,25 @@ func TestInstall(t *testing.T) {
 		panic(err)
 	}
 
-	assert.Contains(t, installRes.Output, "to_install=11 to_update=0")
-	assert.Contains(t, installRes.Output, "Library path to install packages: test-library")
+	t.Run("should install 11", func(t *testing.T) {
+		assert.Contains(t, installRes.Output, "to_install=11 to_update=0")
+	})
+	t.Run("should install to the test library", func(t *testing.T) {
+		assert.Contains(t, installRes.Output, "Library path to install packages: test-library")
+	})
+
 
 	testCmd := command.New(command.WithDir("Rscripts"))
 	// we could also suppress any global state by doing things like setting --vanilla
 	// then setting the R_LIBS_SITE using command.WithEnv() however
 	// this way it will be easy to interactively develop the script in the Rproj,
 	// then invoke it whenever needed
-	testRes, err := testCmd.Run(ctx, "Rscript", "--quiet", "install_test.R")
-	if err != nil {
-		panic(err)
-	}
-	g := goldie.New(t)
-	g.Assert(t, "install", []byte(testRes.Output))
+	t.Run("install the packages and dependencies", func(t *testing.T) {
+		testRes, err := testCmd.Run(ctx, "Rscript", "--quiet", "install_test.R")
+		if err != nil {
+		  	t.Fatalf("failed to run Rscript command with err: %s", err)
+		}
+		g := goldie.New(t)
+		g.Assert(t, "install", []byte(testRes.Output))
+	})
 }
