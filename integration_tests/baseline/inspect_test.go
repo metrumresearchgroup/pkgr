3008a6588d63d7ddaca466d5c14a9af9c7fa7a5a
Devin Pastoor <devinp@metrumrg.com>
2021-07-17 15:24:21 -0400

    test: add additional run tests and invocation by make

--- integration_tests/baseline/inspect_test.go
+++ integration_tests/baseline/inspect_test.go
@@ -2,6 +2,7 @@ package baseline
 
 import (
 	"context"
+	"encoding/json"
 	"github.com/metrumresearchgroup/command"
 	"github.com/sebdah/goldie/v2"
 	"testing"
@@ -11,11 +12,23 @@ func TestInspectDeps(t *testing.T) {
 	testCmd := command.New()
 	ctx := context.TODO()
 	res, err := testCmd.Run(ctx, "pkgr", "inspect", "--deps", "--json")
+
 	if err != nil {
 		panic(err)
 	}
-	g := goldie.New(t)
-	g.Assert(t, "inspect-deps", []byte(res.Output))
+	t.Run("get dependencies as json", func(t *testing.T) {
+		g := goldie.New(t)
+		g.Assert(t, "inspect-deps", []byte(res.Output))
+	})
+
+	// the situation that can arise is if log messages slip in, so the output would be some logrus message + json
+	t.Run("dependencies are valid json", func(t *testing.T) {
+		jsonMap := make(map[string]interface{})
+		err := json.Unmarshal([]byte(res.Output), &jsonMap)
+		if err != nil {
+			t.Errorf("could not unmarshal dependency json with error %s", err)
+		}
+	})
 }
 
 func TestInspectReverseDeps(t *testing.T) {
@@ -25,7 +38,17 @@ func TestInspectReverseDeps(t *testing.T) {
 	if err != nil {
 		panic(err)
 	}
-	g := goldie.New(t)
-	g.Assert(t, "inspect-reverse-deps", []byte(res.Output))
+	t.Run("reverse dependencies are valid json", func(t *testing.T) {
+		jsonMap := make(map[string]interface{})
+		err := json.Unmarshal([]byte(res.Output), &jsonMap)
+		if err != nil {
+			t.Errorf("could not unmarshal reverse dependency json with error %s", err)
+		}
+	})
+	// the situation that can arise is if log messages slip in, so the output would be some logrus message + json
+	t.Run("get reverse dependencies as json", func(t *testing.T) {
+		g := goldie.New(t)
+		g.Assert(t, "inspect-reverse-deps", []byte(res.Output))
+	})
 }
 
