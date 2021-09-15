e9a61901a42c975001707e6fbd2f653872481106
Eli Sarver <elis@metrumrg.com>
2021-09-15 10:59:16 -0400

    Suppress output of automaxprocs.

--- cmd/pkgr/pkgr.go
+++ cmd/pkgr/pkgr.go
@@ -1,9 +1,14 @@
 package main
 
 import (
+	"fmt"
+	"os"
+
+	"go.uber.org/automaxprocs/maxprocs"
+
 	"github.com/metrumresearchgroup/pkgr/cmd"
 
-	_ "go.uber.org/automaxprocs"
+	_ "go.uber.org/automaxprocs/maxprocs"
 )
 
 // if want to generate docs
@@ -13,5 +18,14 @@ import (
 //		panic(err)
 //	}
 func main() {
+	setGOMAXPROCS()
 	cmd.Execute()
+}
+
+func setGOMAXPROCS() {
+	// Silently set the maxprocs
+	discard := func(string, ...interface{}) {}
+	if _, err := maxprocs.Set(maxprocs.Logger(discard)); err != nil {
+		_, _ = fmt.Fprintf(os.Stderr, "unable to set maxprocs: %v", err)
+	}
 }
