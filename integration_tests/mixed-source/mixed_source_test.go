68d2029f750d1581ee11cbce6ae12f5b1cf79a1f
seth127 <seth127@gmail.com>
2022-01-12 16:29:45 -0500

    test: removing unneeded testify/assert from mixed_source_test.go

--- integration_tests/mixed-source/mixed_source_test.go
+++ integration_tests/mixed-source/mixed_source_test.go
@@ -7,7 +7,7 @@ import (
 
 	"github.com/metrumresearchgroup/command"
 	"github.com/sebdah/goldie/v2"
-	"github.com/stretchr/testify/assert"
+	//"github.com/stretchr/testify/assert"
 
 	. "github.com/metrumresearchgroup/pkgr/testhelper"
 )
