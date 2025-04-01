9689f06dc0a087b23570bd2cc3ea494508abe9e4
Kyle Meyer <kylem@metrumrg.com>
2025-04-01 13:29:54 -0400

    install_test.go: use pillar instead of testthat as tarball input
    TestTarballInstall has a test case that confirms that a library is
    correctly set up when there's a tarball specified for a package that
    has dependencies.  It uses testthat 2.1.1 as the input tarball.
    
    Compiling testthat 2.1.1 on Ubuntu 22.04 fails with
    
      ../inst/include/testthat/vendor/catch.h:6490:33: error: size of array ‘altStackMem’ is not an integral constant-expression
       6490 |         static char altStackMem[SIGSTKSZ];
            |                                 ^~~~~~~~
      ../inst/include/testthat/vendor/catch.h:6541:45: error: size of array ‘altStackMem’ is not an integral constant-expression
       6541 |     char FatalConditionHandler::altStackMem[SIGSTKSZ] = {};
            |                                             ^~~~~~~~
    
    testthat 3.1.0 fixes that failure, so we could update the
    localrepos/tarball-deps repo to use testthat 3.1.0 or later.  The
    downside of that is that testthat 3.1.0 specifies 21 packages for its
    Imports versus the 9 specified by testthat 2.1.1.  In addition to
    further bloating the Git repository (our tests would ideally be going
    in the other direction), adding more packages increases the odds of
    hitting into incompatibilities with OS and R version updates.
    
    Instead rearrange things to work with the source tarballs that we
    already track while maintaining the core features of the test.  In
    particular, the test should confirm that dependencies of the specified
    tarball are installed to the library.  There is also a package
    specified in the normal "Packages" set.  The dependencies of the
    Packages package should not include all the dependencies of the
    Tarballs package because then it would not be clear that the tarball
    led to the dependency being installed in the library.
    
    Use pillar (which has several dependencies) as the Tarballs package
    and openssl as the Packages package (which pulls in askpass and
    indirectly sys, neither of which are pulled in by pillar).  Drop now
    unnecessary packages from the repo and regenerate PACKAGES* with
    tools::write_PACKAGES (under R 4.4).

--- cmd/install_test.go
+++ cmd/install_test.go
@@ -202,25 +202,22 @@ func TestTarballInstall(t *testing.T) {
 			installUpdates:  false,
 			installSuggests: false,
 			toInstall: []string{
-				"pillar",
+				"openssl",
 			},
 			toInstallTarballs: []string{
-				filepath.Join(localReposDir, "tarballs", "testthat_2.1.1.tar.gz"),
+				filepath.Join(localReposDir, "tarballs", "pillar_1.3.1.tar.gz"),
 			},
 			expectedInstalled: []string{
+				"askpass", // Pulled in by openssl.
 				"assertthat",
 				"cli",
 				"crayon",
-				"digest",
 				"fansi",
-				"magrittr",
+				"openssl",
 				"pillar",
-				"praise",
-				"R6",
 				"rlang",
-				"testthat", //Should be installed through tarball
+				"sys", // Pulled in by openssl.
 				"utf8",
-				"withr",
 			},
 		},
 		// Right after release 1.0.0, we found a bug on a specific tarball.
